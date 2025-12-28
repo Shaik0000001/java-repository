c0e5e5c90ebade2babefb95871cd9e256c679a05
Shaik Mahaboobbasha <mahaboobbashashaik671@gmail.com>
2025-12-28 13:45:35 +0530

    Add new.java with main method and variables

---
+++ new.java
@@ -0,0 +1,15 @@
+class new {
+    public static void main(String[] args) {
+        int age = 20;
+        float marks = 85.5f;
+        double salary = 45000.75;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println(age);
+        System.out.println(marks);
+        System.out.println(salary);
+        System.out.println(grade);
+        System.out.println(isStudent);
+    }
+}
